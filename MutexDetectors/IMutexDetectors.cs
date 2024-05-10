1c3d4c64d3e4b9bfdb397e75a3b2d259913c5c86
Kristian Skov Johansen <kris701kj@gmail.com>
2024-05-10 12:35:33 +0200

    Add project files.

---
+++ MutexDetectors/IMutexDetectors.cs
@@ -0,0 +1,10 @@
+﻿using PDDLSharp.Models.PDDL;
+using PDDLSharp.Models.PDDL.Expressions;
+
+namespace MutexDetectors
+{
+    public interface IMutexDetectors
+    {
+        public List<PredicateExp> FindMutexes(PDDLDecl decl);
+    }
+}
